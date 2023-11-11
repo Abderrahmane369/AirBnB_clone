147c6a46cd49ac673914358483d032929a4983f9
Abderrahmane369 <cszamasu@gmail.com>
2023-11-11 19:56:24 +0100

    New_update

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -51,3 +51,22 @@ class TestBaseModel(unittest.TestCase):
 
         base_ = {'my_number': 23, 'name': 'AOAOAO', '__class__': 'BaseModel', 'updated_at': base.updated_at.isoformat(), 'id': base.id, 'created_at': base.created_at.isoformat()}
         self.assertEqual(base.to_dict(), base_)
+
+    def test_kwargs_args(self):
+        dd = {'my_number': 23, 'name': 'AOAOAO', 'updated_at': datetime.datetime.now().isoformat(), 'id': '56d43177-cc5f-4d6c-a0c1-e167f8c27337', 'created_at': datetime.datetime.now().isoformat()}
+
+        base = BaseModel(**dd)
+
+        self.assertEqual(base.my_number, dd['my_number'])
+        self.assertEqual(base.name, dd['name'])
+        self.assertEqual(base.updated_at.isoformat(), dd['updated_at'])
+        self.assertEqual(base.created_at.isoformat(), dd['created_at'])
+        self.assertEqual(base.id, dd['id'])
+
+        my_model = BaseModel()
+        my_model.name = "My_First_Model"
+        my_model.my_number = 89
+
+        self.assertRegex(my_model.id, r'^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$')
+        self.assertRegex(my_model.created_at.isoformat(), r'^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{6}$')
+        self.assertRegex(my_model.updated_at.isoformat(), r'^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{6}$')
