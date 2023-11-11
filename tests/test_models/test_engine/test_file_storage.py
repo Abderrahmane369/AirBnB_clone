b3ab048a4e7aa213e126bf9b9059efaba1630c34
Abderrahmane369 <cszamasu@gmail.com>
2023-11-11 15:12:57 +0100

    New_update

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Modules"""
+import unittest
+from ...models.engine.file_storage import FileStorage
+
+
+class TestFileStorage(unittest.TestCase):
+    """Let's test"""
+    pass
+
