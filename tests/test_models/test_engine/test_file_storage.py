c26f46f44c8c383810e03b361f981e23e5585ebc
Abderrahmane369 <cszamasu@gmail.com>
2023-11-12 00:42:30 +0100

    New_update

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 """Modules"""
-import unittest
-from models.engine.file_storage import FileStorage
-from models import storage
-import os
 from models.base_model import BaseModel
 
 
