d437aca748aeb5cab2b5c9e121529399dea5fb7f
Abderrahmane369 <cszamasu@gmail.com>
2023-11-11 15:23:02 +0100

    New_update

---
+++ tests/test_console.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Modules"""
+import unittest
+from ... import console
+
+
+class TestConsole(unittest.TestCase):
+    """Testing..."""
+    pass
+
---
+++ tests/test_models/test_user.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Modules"""
+import unittest
+from ...models.user import User
+
+
+class TestUser(unittest.TestCase):
+    """Testing..."""
+    pass
+
